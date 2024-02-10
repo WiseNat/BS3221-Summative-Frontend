e1d4d3ca98baccdb14071c14274f3ba750c918a0
WiseNat <nathan88wise@gmail.com>
2024-02-10 15:28:43 +0000

    Added skeleton API

---
+++ src/routes/pet/+server.js
@@ -0,0 +1,25 @@
+import { json } from '@sveltejs/kit';
+
+// TODO: Get pet(s)
+export function GET() {
+
+	return json("TODO");
+}
+
+// TODO: Delete pet
+export function DELETE() {
+
+	return json("TODO");
+}
+
+// TODO: Add new pet
+export function POST() {
+
+	return json("TODO");
+}
+
+// TODO: Update pet
+export function PATCH() {
+
+	return json("TODO");
+}
---
+++ src/routes/user/+server.js
@@ -0,0 +1,19 @@
+import { json } from '@sveltejs/kit';
+
+// TODO: Get user(s)
+export function GET() {
+
+	return json("TODO");
+}
+
+// TODO: Add new user
+export function POST() {
+
+	return json("TODO");
+}
+
+// TODO: Update user
+export function PATCH() {
+
+	return json("TODO");
+}
