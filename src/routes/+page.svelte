8cba1f32d775fbc4f39dad3c1e3f3e8ce9059d3b
WiseNat <nathan88wise@gmail.com>
2024-02-29 13:32:24 +0000

    Upgraded frontend form visuals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,85 +1,83 @@
 <style>
-    form  {
-        display: table;
+    :root {
+        margin: 3em 6em;
     }
 
-    div {
-        display: table-row;
+    input[type=text], input[type=email], input[type=tel], input[type=date], select {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        box-sizing: border-box;
+        border: 1px solid gray;
+        border-radius: 4px;
     }
 
-    label {
-        display: table-cell;
-        padding: 5px;
-    }
-    
-    input {
-        display: table-cell; 
+    input[type=submit] {
         width: 100%;
+        padding: 14px 20px;
+        margin: 20px 0;
+        background-color: var(--blue-sapphire);
+        color: var(--snow);
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
     }
-    
-    select {
-        display: table-cell;
-        width: 100%;
+
+    input[type=submit]:hover {
+        background-color: var(--yankees-blue);
+    }
+
+    br {
+        margin: 0 0 30px 0;
+    }
+
+    hr {
+        color: var(--yankees-blue);
+        margin: 50px 0 80px 0;
     }
 </style>
 
-<h1>Register a Pet</h1>
+<h1>Register a Dog</h1>
 <form method="POST">
-    <div>
-        <label for="name">Name </label>
-        <input type="text" name="name" id="name" required />
-    </div>
-    <div>
-        <label for="owner">Owner </label>
-        <input type="owner" name="owner" id="owner" required />
-    </div>
-    <div>
-        <label for="breed">Breed </label>
-        <input type="breed" name="breed" id="breed" required />
-    </div>
-    <div>
-        <label for="gender">Gender </label>
-        <select name="gender" id="gender-select">
-            <option value="">--Please choose an option--</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-        </select>
-    <div>
+    <label for="name">Name </label>
+    <input type="text" name="name" id="name" required />
     <br>
-    <div>
-        <input type="submit" value="Submit"/>
-    </div>
+    <label for="owner">Owner </label>
+    <input type="text" name="owner" id="owner" required />
+    <br>
+    <label for="breed">Breed </label>
+    <input type="text" name="breed" id="breed" required />
+    <br>
+    <label for="gender">Gender </label>
+    <select name="gender" id="gender-select">
+        <option value="">--Please choose an option--</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+    </select>
+    <br>
+    <input type="submit" value="Submit"/>
 </form>
-<br>
 <hr>
 <h1>Register a Walker</h1>
 <form method="POST">
-    <div>
-        <label for="name">First Name </label>
-        <input type="text" name="name" id="name" required/>
-    </div>
-    <div>
-        <label for="name">Last Name </label>
-        <input type="text" name="name" id="name" required/>
-    </div>
-    <div>
-        <label for="email">Email </label>
-        <input type="email" name="email" id="email" required/>
-    </div>
-    <div>
-        <label for="town-city">Town/City </label>
-        <input type="text" name="town-city" id="town-city" required/>
-    </div>
-    <div>
-        <label for="contact-number">Contact Number </label>
-        <input type="tel" name="contact-number" id="contact-number" required/>
-    </div>
-        <div>
-        <label for="dob">Date of Birth </label>
-        <input type="date" name="dob" id="dob" required/>
-    </div>
+    <label for="name">First Name </label>
+    <input type="text" name="name" id="name" required/>
     <br>
-    <div>
-        <input type="submit" value="Submit"/>
-    </div>
+    <label for="name">Last Name </label>
+    <input type="text" name="name" id="name" required/>
+    <br>
+    <label for="email">Email </label>
+    <input type="email" name="email" id="email" required/>
+    <br>
+    <label for="town-city">Town/City </label>
+    <input type="text" name="town-city" id="town-city" required/>
+    <br>
+    <label for="contact-number">Contact Number </label>
+    <input type="tel" name="contact-number" id="contact-number" required/>
+    <br>
+    <label for="dob">Date of Birth </label>
+    <input type="date" name="dob" id="dob" required/>
+    <br>
+    <input type="submit" value="Submit"/>
 </form>
